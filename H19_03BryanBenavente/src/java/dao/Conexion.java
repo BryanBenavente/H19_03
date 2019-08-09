5864f5269eab3dead6b9fd32470728c67d485a94
Bryan.Benavente <bryan.benavente@vallegrande.edu.pe>
2019-08-09 13:51:59 -0500

    Conexión a BD cloud

--- H19_03BryanBenavente/src/java/dao/Conexion.java
+++ H19_03BryanBenavente/src/java/dao/Conexion.java
@@ -11,10 +11,10 @@ public class Conexion {
     public Connection conectar(){
         try {
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-//            cn = DriverManager.getConnection("jdbc:sqlserver://bryanvg.database.windows.net; databaseName=VENTA;"
-//                    + "user=bryanbenavente; password=Bryanbc17");
-            cn = DriverManager.getConnection("jdbc:sqlserver://LAPTOP-D35JLBTN; databaseName=VENTA;"
-                    + "user=sa; password=bryan");
+            cn = DriverManager.getConnection("jdbc:sqlserver://hachaton.database.windows.net; databaseName=VENT;"
+                    + "user=admn; password=ads-2019");
+//            cn = DriverManager.getConnection("jdbc:sqlserver://LAPTOP-D35JLBTN; databaseName=VENTA;"
+//                    + "user=sa; password=bryan");
         } catch (Exception e) {
             System.out.println("Error al conectar " + e.getMessage());
         }
